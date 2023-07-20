9eed6862233470b3b4eb45f378258e1fa7092fc8
Joseph Haase <joseph.haase.dev@gmail.com>
2023-07-19 23:53:33 -0300

    Adds more settings

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,19 @@ import dotenv from 'dotenv';
 import 'module-alias/register';
 import moduleAlias from 'module-alias';
 import * as path from 'path';
-
-moduleAlias.addAlias('@', path.join(__dirname, 'src'));
+import router from './routes';
 
 dotenv.config();
+moduleAlias.addAlias('@', path.join(__dirname, 'src'));
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.listen(PORT, (): void => console.log(`Server running on http://localhost:${PORT}`));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.listen(port, () => {
-	console.log(`Servidor rodando em http://localhost:${port}`);
-});
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+app.use('/', router);
